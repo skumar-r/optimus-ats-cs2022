d9b03cae5f8927cbf27889ca270ce7171490d0f9
Anish Kumar <itsanishmail@gmail.com>
2022-07-21 11:46:42 +0530

    Updated View

--- ats-ui/src/App.svelte
+++ ats-ui/src/App.svelte
@@ -1,8 +1,19 @@
+<script lang="js">
+  import { Router, Route, Link } from "svelte-navigator";
+  import EmployeeList from "./lib/EmployeeList.svelte";
+  import Dashboard from "./lib/Dashboard.svelte";
+  import EmployeeNew from "./lib/EmployeeNew.svelte";
+  import VehicleList from "./lib/VehicleList.svelte";
+  import VehicleNew from "./lib/VehicleNew.svelte";
+  import VehicleVerify from "./lib/VehicleVerify.svelte";
+  import EmployeeVerify from "./lib/EmployeeVerify.svelte";
+</script>
+
 <Router>
-  <nav>
-    <Link to="/">Home</Link>    
+  <nav class="dashboardButton">
+    <Link class="dashboardButtonLink" to="/">Dashboard</Link>
   </nav>
-  <Route path='/'><Dashboard /></Route>
+  <Route path="/"><Dashboard /></Route>
   <Route path="employeeList"><EmployeeList /></Route>
   <Route path="employeeNew"><EmployeeNew /></Route>
   <Route path="employeeVerify"><EmployeeVerify /></Route>
@@ -10,13 +21,3 @@
   <Route path="vehicle/new"><VehicleNew /></Route>
   <Route path="vehicle/verify"><VehicleVerify /></Route>
 </Router>
-<script lang="js">  
-  import { Router, Route, Link } from "svelte-navigator";
-  import EmployeeList from './lib/EmployeeList.svelte';
-  import Dashboard from './lib/Dashboard.svelte';
-  import EmployeeNew from './lib/EmployeeNew.svelte';
-  import VehicleList from './lib/VehicleList.svelte';
-  import VehicleNew from './lib/VehicleNew.svelte';
-  import VehicleVerify from './lib/VehicleVerify.svelte';
-  import EmployeeVerify from './lib/EmployeeVerify.svelte';
-</script>
