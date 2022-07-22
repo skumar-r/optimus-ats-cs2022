c56317a6da51e3fde047c86a3a8b6c6f710dafbc
Anish Kumar <itsanishmail@gmail.com>
2022-07-22 11:39:01 +0530

    Updated with Registration Api Call

--- ats-ui/src/lib/EmployeeNew.svelte
+++ ats-ui/src/lib/EmployeeNew.svelte
@@ -1,18 +1,44 @@
 <script>
+  // @ts-nocheck
+
   import { createForm } from "svelte-forms-lib";
   import Paper, { Title, Subtitle, Content } from "@smui/paper";
-  let empPhoto, idPhoto, empPhotoInput, idPhotoInput;
+  let empPhoto =
+    "https://digitalfinger.id/wp-content/uploads/2019/12/no-image-available-icon-6.png";
+  let idPhoto =
+    "https://digitalfinger.id/wp-content/uploads/2019/12/no-image-available-icon-6.png";
+  let empPhotoInput, idPhotoInput;
+
   const { form, handleChange, handleSubmit } = createForm({
     initialValues: {
       title: "",
       name: "",
       email: "",
       department: "",
-      empPhoto: undefined
+      empPhoto: "",
     },
     onSubmit: (values) => {
-      debugger;
-      alert(JSON.stringify(values));
+      const dataArray = new FormData();
+      dataArray.append("title", values.title);
+      dataArray.append("employeeName", values.name);
+      dataArray.append("department", values.department);
+      dataArray.append("designation", "Manager");
+      dataArray.append("email", values.email);
+      dataArray.append("mobile", "9876543210");
+      dataArray.append("hasS3Photo", false);
+      dataArray.append("photoFrontFile", empPhotoInput);
+      dataArray.append("photoIdCardFile", idPhotoInput);
+      fetch("http://localhost:9010/employee", {
+        method: "POST",
+        headers: [["Content-Type", "multipart/form-data"]],
+        body: dataArray,
+      })
+        .then((response) => {
+          // Successfully uploaded
+        })
+        .catch((error) => {
+          // Upload failed
+        });
     },
   });
 
@@ -30,123 +56,131 @@
     let reader = new FileReader();
     reader.readAsDataURL(image);
     reader.onload = (e) => {
+      // @ts-ignore
       idPhoto = e.target.result;
     };
   };
 </script>
+
 <div>
-<div class="paper-container">
-  <Paper color="primary" variant="outlined" class="mdc-theme--primary">
-    <Title>Add a New Employee</Title>
-    <Content>
-      <form on:submit={handleSubmit}>
-        <label for="title">Title</label>
-        <select
-          id="title"
-          name="title"
-          on:change={handleChange}
-          bind:value={$form.title}
-        >
-          <option />
-          <option>Mr.</option>
-          <option>Mrs.</option>
-          <option>Mx.</option>
-        </select>
+  <div class="paper-container">
+    <Paper color="primary" variant="outlined" class="mdc-theme--primary" style="margin-top:25px;">
+      <Title>Add a New Employee</Title>
+      <Content>
+        <form on:submit={handleSubmit} style="height: 440px;">
+          <div style="width:33%;float:left;">
+            <label for="title">Title</label>
+            <select
+              id="title"
+              name="title"
+              on:change={handleChange}
+              bind:value={$form.title}
+            >
+              <option />
+              <option>Mr.</option>
+              <option>Mrs.</option>
+              <option>Mx.</option>
+            </select>
 
-        <label for="name">Name</label>
-        <input
-          id="name"
-          name="name"
-          on:change={handleChange}
-          bind:value={$form.name}
-        />
+            <label for="name">Name</label>
+            <input
+              id="name"
+              name="name"
+              on:change={handleChange}
+              bind:value={$form.name}
+            />
 
-        <label for="email">Email Address</label>
-        <input
-          id="email"
-          name="email"
-          on:change={handleChange}
-          bind:value={$form.email}
-        />
+            <label for="email">Email Address</label>
+            <input
+              id="email"
+              name="email"
+              on:change={handleChange}
+              bind:value={$form.email}
+            />
 
-        <label for="department">Department</label>
-        <select
-          id="department"
-          name="department"
-          on:change={handleChange}
-          bind:value={$form.department}
-        >
-          <option />
-          <option>Technology</option>
-          <option>Research</option>
-          <option>IT Support</option>
-          <option>Sales & Support</option>
-          <option>Security</option>
-          <option>Facilities</option>
-        </select>
+            <label for="department">Department</label>
+            <select
+              id="department"
+              name="department"
+              on:change={handleChange}
+              bind:value={$form.department}
+            >
+              <option />
+              <option>Technology</option>
+              <option>Research</option>
+              <option>IT Support</option>
+              <option>Sales & Support</option>
+              <option>Security</option>
+              <option>Facilities</option>
+            </select>
+          </div>
 
-        <label for="employeeImage">Employee Photo</label>
-        <img class="avatar" src={empPhoto} alt="d" />
-        <img
-          class="upload"
-          src="https://static.thenounproject.com/png/625182-200.png"
-          alt=""
-          on:click={() => {
-            empPhotoInput.click();
-          }}
-        />
-        <div
-          class="chan"
-          on:click={() => {
-            empPhotoInput.click();
-          }}
-        >
-          Choose Image
-        </div>
-        <input
-          name="employeeImage"
-          id="employeeImage"
-          style="display:none"
-          type="file"
-          accept=".jpg, .jpeg, .png"
-          on:change={(e) => onFileSelectedEmpPhoto(e)}
-          bind:this={empPhotoInput}
-        />
-
-        <label for="idcardImage">ID Card Photo</label>
-        <img class="avatar" src={idPhoto} alt="d" />
-        <img
-          class="upload"
-          src="https://static.thenounproject.com/png/625182-200.png"
-          alt=""
-          on:click={() => {
-            idPhotoInput.click();
-          }}
-        />
-        <div
-          class="chan"
-          on:click={() => {
-            idPhotoInput.click();
-          }}
-        >
-          Choose Image
-        </div>
-        <input
-          name="idcardImage"
-          id="idcardImage"
-          style="display:none"
-          type="file"
-          accept=".jpg, .jpeg, .png"
-          on:change={(e) => onFileSelectedIdPhoto(e)}
-          bind:this={idPhotoInput}
-        />
-        <button type="submit">Submit</button>
-      </form>
-    </Content>
-  </Paper>
+          <div style="width:33%;float:left;padding: 0 5px;">
+            <label for="employeeImage">Employee Photo</label>
+            <img class="avatar" src={empPhoto} alt="d" />
+            <img
+              class="upload"
+              src="https://static.thenounproject.com/png/625182-200.png"
+              alt=""
+              on:click={() => {
+                empPhotoInput.click();
+              }}
+            />
+            <div
+              class="chan"
+              on:click={() => {
+                empPhotoInput.click();
+              }}
+            >
+              Choose Image
+            </div>
+            <input
+              name="employeeImage"
+              id="employeeImage"
+              style="display:none"
+              type="file"
+              accept=".jpg, .jpeg, .png"
+              on:change={(e) => onFileSelectedEmpPhoto(e)}
+              bind:this={empPhotoInput}
+            />
+          </div>
+          <div style="width:33%;float:left;">
+            <label for="idcardImage">ID Card Photo</label>
+            <img class="avatar" src={idPhoto} alt="d" />
+            <img
+              class="upload"
+              src="https://static.thenounproject.com/png/625182-200.png"
+              alt=""
+              on:click={() => {
+                idPhotoInput.click();
+              }}
+            />
+            <div
+              class="chan"
+              on:click={() => {
+                idPhotoInput.click();
+              }}
+            >
+              Choose Image
+            </div>
+            <input
+              name="idcardImage"
+              id="idcardImage"
+              style="display:none"
+              type="file"
+              accept=".jpg, .jpeg, .png"
+              on:change={(e) => onFileSelectedIdPhoto(e)}
+              bind:this={idPhotoInput}
+            />
+          </div>
+          <div style="display: flex;width:100%;justify-content: center;">
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </Content>
+    </Paper>
+  </div>
 </div>
-</div>
-
 
 <style>
   .upload {
